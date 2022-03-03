e43d143a695b187f9fdaeeda496b02a060bc72b9
shuukhratbegmatov <shuukhratbegmatov@gmail.com>
2022-03-03 18:41:51 +0500

    'snsa

---
+++ precache-manifest.c8de66e43f5bd648a5c78dc3720b9de5.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "870ca3e5a261c3822405",
+    "url": "/css/app.44ba5231.css"
+  },
+  {
+    "revision": "9934f56e82f662ed72ce",
+    "url": "/css/chunk-vendors.50ac4f62.css"
+  },
+  {
+    "revision": "97b4f35d75a4e0b694f6db83c45197f2",
+    "url": "/daak.png"
+  },
+  {
+    "revision": "bf59c687bc6d3a70204d3944082c5cc0",
+    "url": "/fonts/Poppins-Medium.bf59c687.ttf"
+  },
+  {
+    "revision": "766d4e948a3d8790a984ca28a979474a",
+    "url": "/img/gov.766d4e94.png"
+  },
+  {
+    "revision": "b57df73ada8a8067f71a847d8aff6f37",
+    "url": "/img/image12.b57df73a.png"
+  },
+  {
+    "revision": "d3b6ddd07447477d11e796d405d419bd",
+    "url": "/img/image13.d3b6ddd0.png"
+  },
+  {
+    "revision": "976ecbf0dccff8c893970ba0eef779da",
+    "url": "/img/it.976ecbf0.png"
+  },
+  {
+    "revision": "1c80a17a74a6a32d3b9c051815c6362c",
+    "url": "/img/mc.1c80a17a.png"
+  },
+  {
+    "revision": "a0be9b52de8e6e9ee5ea040a85ef6504",
+    "url": "/img/uzlogo_white.a0be9b52.png"
+  },
+  {
+    "revision": "b3f5c757b31b934330de2fab025d8311",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2bcb99730d33c78815f9",
+    "url": "/js/about.dd768651.js"
+  },
+  {
+    "revision": "870ca3e5a261c3822405",
+    "url": "/js/app.1b8d7c52.js"
+  },
+  {
+    "revision": "9934f56e82f662ed72ce",
+    "url": "/js/chunk-vendors.0e1ec122.js"
+  },
+  {
+    "revision": "0fb73d95c4078d90354cbb9e5f5bb1a5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
